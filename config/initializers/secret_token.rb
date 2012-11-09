abaddc150b1f011f5dd915113d2655ba4fc14272
Kai Kontio <kai@virtualyze.tietoteollisuus.ja>
2012-11-09 08:19:51 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lounas::Application.config.secret_token = '6b2c4d6be123e1b7a0acf625fe4cb2487667eb8443f0fb7064b6dc63c3a5bb1ae4e527c347821e70c48e320af1386fb2c277d2ca1303d0630b3bcc4083489d1a'
