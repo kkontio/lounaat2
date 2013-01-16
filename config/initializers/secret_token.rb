0ac6437d55c1e38b9fcc4ae1809f31a91f7cd021
Kai Kontio <kai.kontio@gmail.com>
2013-01-16 17:24:14 +0200

    Update Rails, Ignore RubyMine

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lounas::Application.config.secret_token = '6b2c4d6be123e1b7a0acf625fe4cb2487667eb8443f0fb7064b6dc63c3a5bb1ae4e527c347821e70c48e320af1386fb2c277d2ca1303d0630b3bcc4083489d1a'
+Lounas::Application.config.secret_token = 'ad6f0067fc5d147ef0d10de68857bb0ded50556510cb355f853c144fcf7a716beac0e49fe6b95b098a6607346aa00b35d326d2ab194f2f3cc03e81b0c0e0ce50'
