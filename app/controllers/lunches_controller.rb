e62ac033d5f70acfe11892d68a094ca8b81ebc18
Kai Kontio <kai.kontio@gmail.com>
2013-02-04 23:00:22 +0200

    Playing around with some hashing.

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,5 +1,19 @@
 class LunchesController < ApplicationController
   def index
-    @lunches = Lunch.where("date >= ?", Date.today)
+    lunch_days = Lunch.select(:date).uniq.where("date >= ?", Date.today).order("date")
+    h = {}
+    lunch_days.each do |d|
+      h[d.date] = Lunch.includes(:restaurant).where("date = ?", d.date).order("restaurant_id")
+    end
+
+    h.each_pair do |k,v|
+      puts k.to_s
+      v.each do |v|
+        puts v.restaurant.name
+      end
+    end
+
+
+    @lunches = Lunch.includes(:restaurant).where("date >= ?", Date.today).order("date")
   end
 end
