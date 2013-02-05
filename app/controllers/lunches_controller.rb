dcf328a3ee854d88163d07c2186ad82b09ebf1a1
Kai Kontio <kai.kontio@gmail.com>
2013-02-05 18:06:02 +0200

    Model and controller methods for lunches.

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,19 +1,5 @@
 class LunchesController < ApplicationController
   def index
-    lunch_days = Lunch.select(:date).uniq.where("date >= ?", Date.today).order("date")
-    h = {}
-    lunch_days.each do |d|
-      h[d.date] = Lunch.includes(:restaurant).where("date = ?", d.date).order("restaurant_id")
-    end
-
-    h.each_pair do |k,v|
-      puts k.to_s
-      v.each do |v|
-        puts v.restaurant.name
-      end
-    end
-
-
-    @lunches = Lunch.includes(:restaurant).where("date >= ?", Date.today).order("date")
+    @lunches_by_date = Lunch.lunches_by_date
   end
 end
--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,4 +2,19 @@ class Lunch < ActiveRecord::Base
   attr_accessible :date, :description, :restaurant_id
   validates :date, :uniqueness => { :scope => :restaurant_id }
   belongs_to :restaurant
+
+  def self.lunches_by_date()
+    lunches = Lunch.includes(:restaurant).where("date >= ?", Date.today).order("date")
+
+    h = {}
+
+    lunches.each do |l|
+      h[l.date] ||= []
+      h[l.date] << l
+    end
+
+    h.values.each { |v| v.sort! { |a,b| a.restaurant.name <=> b.restaurant.name } }
+
+    return h
+  end
 end
