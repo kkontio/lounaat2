6a24ebe883758dc557c0a2d629c677f16760ff20
Kai Kontio <kai.kontio@gmail.com>
2013-03-21 18:54:33 +0200

    Move the secret token to environment variable.
    In preparation for actual features which make use of sessions.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lounas::Application.config.secret_token = 'ad6f0067fc5d147ef0d10de68857bb0ded50556510cb355f853c144fcf7a716beac0e49fe6b95b098a6607346aa00b35d326d2ab194f2f3cc03e81b0c0e0ce50'
+Lounas::Application.config.secret_token = ENV['SECRET_TOKEN']
