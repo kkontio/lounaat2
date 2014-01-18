d1284bf7e8db8e47b634ff89f691a961bb7b0284
Kai Kontio <kai.kontio@gmail.com>
2014-01-18 20:14:07 +0200

    A bug in scraper date handling (surprise, surprise).
    Still ugly as hell. But hey, at least it works for now! :)

--- lib/scraper/restaurant_modules/sello_parser.rb
+++ lib/scraper/restaurant_modules/sello_parser.rb
@@ -1,10 +1,10 @@
 module SelloParser
   def parse_sello
     lambda do |doc, url_date|
-      monday = Date.parse('monday')
+      previous_monday = url_date
 
-      if monday > url_date
-        monday -= 7
+      while (previous_monday.wday != 1)
+        previous_monday = previous_monday - 1
       end
 
       parsed_results = {}
@@ -12,7 +12,7 @@ module SelloParser
       doc.css('.lunch_list').each_with_index do |lunch_info, i|
         break if i > 4
 
-        lunch_date = monday + i
+        lunch_date = previous_monday + i
         lunch_items = []
 
         lunch_info.css('li').each do |li|
